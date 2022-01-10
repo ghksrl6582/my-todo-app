ed08ddbe825362266d53afd16bfd2121b3cdb8d2
ghksrl6582 <ghksrl6582@daum.net>
2022-01-10 17:04:14 +0900

    Initial commit

---
+++ src/main/java/com/example/mytodoapp/MyTodoAppApplication.java
@@ -0,0 +1,14 @@
+package com.example.mytodoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
+
+@SpringBootApplication
+public class MyTodoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyTodoAppApplication.class, args);
+	}
+
+}
