03b9a2e6f66e1036e0b599efa82461ef2c8d1e65
ghksrl6582 <ghksrl6582@daum.net>
2022-01-11 14:54:04 +0900

    TodoController, TodoEntity 수정

--- src/main/java/com/example/mytodoapp/controller/TodoController.java
+++ src/main/java/com/example/mytodoapp/controller/TodoController.java
@@ -1,10 +1,33 @@
 package com.example.mytodoapp.controller;
 
+import com.example.mytodoapp.dto.ResponseDTO;
+import com.example.mytodoapp.service.TodoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/todo")
 public class TodoController {
 
+    private final TodoService todoService;
+
+    @GetMapping("/test")
+    public ResponseEntity<?> testTodo() {
+        String str = todoService.testService();
+        List<String> list = new ArrayList<>();
+        list.add(str);
+
+        var response = ResponseDTO.<String>builder()
+                .data(list)
+                .build();
+
+        return ResponseEntity.ok().body(response);
+    }
 }
