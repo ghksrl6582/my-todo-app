8594c822078fe3792511a2dfddfe7dc943f0f917
ghksrl6582 <ghksrl6582@daum.net>
2022-01-12 16:27:58 +0900

    Todo컴포넌트에material-ui 적용

--- todo-frontend/todo-react-app/src/App.js
+++ todo-frontend/todo-react-app/src/App.js
@@ -1,12 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
 import Todo from './Todo';
+import { useState } from 'react';
+import { List, Paper } from '@material-ui/core';
 
-function App() {
+const App = () => {
+
+  const [state, setState] = useState({
+    items: [
+      { id: "0", title: "myWorld1", done: true },
+      { id: "1", title: "myWorld2", done: false },
+      { id: "2", title: "myWorld1", done: true },
+      { id: "3", title: "myWorld2", done: false }
+    ]
+  });
+
+  let todoItems = state.items.length > 0 && (
+    <Paper style={{ margin: 16 }}>
+      <List>
+        {state.items.map((item, idx) => (
+          <Todo item={item} />
+        ))}
+      </List>
+    </Paper>
+  );
+
   return (
-    <div className="App">
-      <Todo /><Todo /><Todo />
-    </div >
+    <div className="App">{todoItems}</div >
   );
 }
 
--- todo-frontend/todo-react-app/src/Todo.js
+++ todo-frontend/todo-react-app/src/Todo.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Checkbox, InputBase, ListItem, ListItemText } from "@material-ui/core";
 
 const Todo = (props) => {
+
+    const [state, setState] = useState({ item: props.item });
+
+    const item = state.item;
     return (
-        <div className="Todo">
-            <input type="checkbox" id="todo0" name="todo0" value="todo0" />
-            <label for="todo0">컴포넌트 만들기</label>
-        </div>
+        <ListItem>
+            <Checkbox checked={item.done} />
+            <ListItemText>
+                <InputBase
+                    inputProps={{ "aria-label": "naked" }}
+                    type="text"
+                    id={item.id}
+                    name={item.id}
+                    value={item.title}
+                    multiline={true}
+                    fullWidth={true}
+                />
+            </ListItemText>
+        </ListItem>
     );
 }
 
